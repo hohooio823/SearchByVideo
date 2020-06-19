cb3894656b5012052262be6f6933a96d2a87a0f0
hohooio823 <hohooio82@gmail.com>
2020-06-20 00:51:14 +0100

    added some fix

--- middlewares/imageSearch.js
+++ middlewares/imageSearch.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-let results = [];
-let count= 0;
+
 const imageSearch = (urls,res)=>{
+  let results = [];
+  let count= 0;
 	const loop = async ()=>{
 		for(let i=0;i<urls.length;i++){
       const response = await axios.get("https://images.google.com/searchbyimage?image_url="+encodeURI(urls[i])+"&encoded_image=&image_content=&filename=&hl=en-US",{ headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; rv:20.0) Gecko/20100101 Firefox/20.0'}  })
@@ -16,16 +17,17 @@ const imageSearch = (urls,res)=>{
 }
   loop().then(()=>{
     let countof={};
+    let finalResults=[]
+    let countResults = [];
     results.forEach(function(item){
     if(!countof[item]) countof[item]=0;
      countof[item]++;//increase
     });
-    const countResults =Object.keys(countof).map(link=>({link,count:countof[link]}));
+    countResults =Object.keys(countof).map(link=>({link,count:countof[link]}));
     countResults.sort((a,b)=>a.count-b.count);
-    let finalResults = countResults.map(url=>url.link,{} )
+    finalResults = countResults.map(url=>url.link,{} )
     console.log(finalResults);
     res.send(finalResults)
-    finalResults = []
   }
   )
 }
